c1e0b42f91e5c491bb0e3b9afe19f61f706ff15a
thanh khiet <cs.thanhkhiet@gmail.com>
2021-08-30 16:59:14 +0700

    deploy to heroku

--- index.js
+++ index.js
@@ -22,18 +22,23 @@ io.on("connection", (socket) => {   // socket in here(socket instance) indicates
         const { error, user } = addUser({id: socket.id, name, room})
         if (error)
             return callback(error)
-        socket.join(room)
-
+            
         socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
         socket.broadcast.to(room).emit('message', { user: 'admin', text: `${name} has joined!` });
+            
+        socket.join(room)
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)})
 
         callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id)
-
+        
         io.to(user.room).emit('message', {user: user.name, text: message})
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+
         callback()
     })
 
